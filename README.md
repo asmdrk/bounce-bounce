b8b22b92502013af845f45818209e5e39fb9754d
diw <dgupta011101@gmail.com>
2026-01-05 00:59:33 +0530

    added support for multiple balls

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bounce-bounce
 just a little bouncing ball simulation. Implemented in python and uses pygame for rendering/game physics.
+
+This version allows for multiple balls to be spawned when the user clicks on screen
--- bounce.py
+++ bounce.py
@@ -13,46 +13,56 @@ def get_theta(x,y):
     theta = math.acos(ratio)
     return theta
 
-def bounce(current_pos : pygame.Vector2, screen: pygame.Surface, radius, dx, dy):
+def bounce(current_pos : pygame.Vector2, screen: pygame.Surface, radius, current_speed: pygame.Vector2):
     # horizontal bounce
     if (current_pos.x + radius) >= screen.get_width() or (current_pos.x - radius) < 0:
-        dx = (-dx * 0.9)
-    if (current_pos.y + radius) >= screen.get_height() or (current_pos.y - radius) < 0:
-        dy = -dy
+        current_speed.x = (-current_speed.x * 0.9) # on a bounce we reduce our speed a littl ebit
     
-    return (dx,dy)
     # vertical bounce
+    if (current_pos.y + radius) >= screen.get_height() or (current_pos.y - radius) < 0:
+        current_speed.y = -current_speed.y
 
+def init_speed():
+    return pygame.Vector2(random.randint(0,100), random.randint(0,50))
+
+def init_pos():
+    return pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1920, 1000))
 clock = pygame.time.Clock()
 running = True
-dx = random.randint(0,100)
-dy = random.randint(0,50)
-current_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+speeds = [pygame.Vector2(random.randint(0,100), random.randint(0,50))]
+positions = [pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)]
 center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
+font = pygame.font.SysFont('Arial', 30) 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    dx,dy = bounce(current_pos, screen, 40, dx, dy)
-    if current_pos.y > 100:
-        dy += gravity
-    print("oof dy:", dy)
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
+    for current_pos, current_speed in zip(positions, speeds):
+        bounce(current_pos, screen, 40, current_speed)
+        if current_pos.y > 100:
+            current_speed.y += gravity
+        # fill the screen with a color to wipe away anything from last frame
 
-    pygame.draw.circle(screen, "red", current_pos, 40)
+        pygame.draw.circle(screen, "red", current_pos, 4)
 
-    current_pos.x += dx
-    current_pos.y += dy
+        current_pos.x += current_speed.x
+        current_pos.y += current_speed.y
 
-    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] or keys[pygame.MOUSEBUTTONUP]:
+        for _ in range(1000):
+            positions.append(init_pos())
+            speeds.append(init_speed())
+
+    # how many circles?
+    text = font.render("Num Circles: " + str(len(positions)), True, (255, 255, 255))  # White text
+    screen.blit(text, (screen.get_width() - text.get_width() - 10, 10))  # Position text in the top-right corner
 
     # flip() the display to put your work on screen
     pygame.display.flip()
